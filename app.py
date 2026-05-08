ee03d9d67800b27af4fea5487424e9319e4e9361
Napolig <napolig@ebrd.com>
2026-05-08 14:51:07 +0000

    Fix uploads log table name 2

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def normalize_mechanism(value):
 
 def log_upload(user_id, mechanism, filename, rows, status, error=None):
     try:
-        supabase.table("upload_log").insert({
+        supabase.table("uploads_log").insert({
             "user_id": user_id,
             "mechanism_id": mechanism,  # per ora usiamo il nome, poi possiamo migliorare
             "filename": filename,
