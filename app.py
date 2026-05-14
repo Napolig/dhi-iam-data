f359ba13a28cbc6a5d9d2070b43c5a3c536624db
Napolig <napolig@ebrd.com>
2026-05-14 14:14:36 +0000

    Changing page title

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import pandas as pd
 # This must stay near the top of the file.
 # =========================================================
 st.set_page_config(page_title="DHI IAM Datahub", layout="wide")
+st.title("DHI IAM Datahub")
 
 
 # =========================================================
